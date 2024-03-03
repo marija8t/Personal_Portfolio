19998dc87781d29072c33c4048406545d5c6ec9f
Marija Travoric <marija8t@gmail.com>
2024-03-02 20:30:56 -0500

    Implementing Hamburger for hidden menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
+//Hamburger for hidden menu
+document.querySelector('.nav-toggle').addEventListener('click', () => {
+    const nav = document.querySelector('.nav');
+    nav.classList.toggle('open'); // Toggle "open" class on and off
+});
 
